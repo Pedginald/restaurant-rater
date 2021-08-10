e79108a079a0eef79673ef1fcc34a9208be5fbf3
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-08-10 17:37:21 -0400

    Testing for nodemon efficacy

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
 	console.log(`Server listening on port ${port}...`);
