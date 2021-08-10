d563649ffd31e2e670b41bf302da32bb841db433
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-08-10 18:21:11 -0400

    Creating get requests for /restaurants

--- server/server.js
+++ server/server.js
@@ -4,6 +4,21 @@ const app = express();
 
 const port = process.env.PORT || 3001;
 
+// Get all restaurants
+app.get('/api/v1/restaurants', (req, res) => {
+	res.status(200).json({
+		status: "success",
+		data: {
+			restaurants: ["mcdonalds", "wendys"]
+		}
+	});
+});
+
+// Get a restaurant
+app.get('/api/v1/restaurants/:id', (req, res) => {
+	console.log(req.params);
+});
+
 app.listen(port, () => {
 	console.log(`Server listening on port ${port}...`);
 });
