a2781345cf4bf37b02161bd4931d6afe1be751d9
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-08-11 16:18:32 -0400

    Testing http requests and importing middleware

--- server/server.js
+++ server/server.js
@@ -1,16 +1,15 @@
 require('dotenv').config();
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use((req, res, next) => {
-	console.log('Yeah our middlewear ran');
-	next();
-});
+// Define middleware at the top of file
+app.use(express.json());
 
 // Get all restaurants
 app.get('/api/v1/restaurants', (req, res) => {
-	console.log('And this route ran too');
+	console.log('This route handler ran');
 	res.status(200).json({
 		status: "success",
 		data: {
@@ -22,11 +21,43 @@ app.get('/api/v1/restaurants', (req, res) => {
 // Get a restaurant
 app.get('/api/v1/restaurants/:id', (req, res) => {
 	console.log(req.params);
+	res.status(200).json({
+		status: "success",
+			data: {
+				restaurant: "mcdonalds"
+			}
+	})
 });
 
 // Create a restaurant
 app.post('/api/v1/restaurants', (req, res) => {
+	console.log(req.body);
+	res.status(201).json({
+		status: "success",
+			data: {
+				restaurant: "mcdonalds"
+			}
+	})
+})
 
+// Update restaurant
+app.put('/api/v1/restaurants/:id', (req, res) => {
+	console.log(req.params);
+	console.log(req.body);
+	res.status(200).json({
+		status: "success",
+		data: {
+			restaurant: "mcdonalds"
+		}
+	})
+})
+
+// Delete restaurant
+app.delete('/api/v1/restaurants/:id', (req, res) => {
+	console.log('Successfully deleted');
+	res.status(204).json({
+		status: "success"
+	})
 })
 
 app.listen(port, () => {
