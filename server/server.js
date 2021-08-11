38373db16d9c49bde8b24bdf89c4f6fd399888c8
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-08-11 11:56:59 -0400

    Adding and testing middleware

--- server/server.js
+++ server/server.js
@@ -1,11 +1,16 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-
 const port = process.env.PORT || 3001;
+
+app.use((req, res, next) => {
+	console.log('Yeah our middlewear ran');
+	next();
+});
 
 // Get all restaurants
 app.get('/api/v1/restaurants', (req, res) => {
+	console.log('And this route ran too');
 	res.status(200).json({
 		status: "success",
 		data: {
@@ -18,6 +23,11 @@ app.get('/api/v1/restaurants', (req, res) => {
 app.get('/api/v1/restaurants/:id', (req, res) => {
 	console.log(req.params);
 });
+
+// Create a restaurant
+app.post('/api/v1/restaurants', (req, res) => {
+
+})
 
 app.listen(port, () => {
 	console.log(`Server listening on port ${port}...`);
