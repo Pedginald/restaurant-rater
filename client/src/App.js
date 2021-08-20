74d706d5d1691d2c733a176060205ec61d27e340
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-08-20 16:05:28 -0400

    Importing react-router-dom and creating routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import RestaurantDetailPage from './routes/RestaurantDetailPage';
+import UpdatePage from './routes/UpdatePage';
+import Home from './routes/Home';
 
 const App = () => {
 	return (
-		<div>App</div>
+		<div>
+			<Router>
+				<Switch>
+					<Route exact path='/' component={Home}/>
+					<Route exact path='/restaurants/:id/update' component={UpdatePage}/>
+					<Route exact path='/restaurants/:id' component={RestaurantDetailPage}/>
+				</Switch>
+			</Router>
+		</div>
 	);
 };
 
