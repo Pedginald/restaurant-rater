a628c78a939dff9ebedf658afb0a60d8bf0ce8e8
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-08-23 16:49:46 -0400

    Invoking cors by adding parenthesis

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 // Define middleware at the top of file
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 
 // Get all restaurants
