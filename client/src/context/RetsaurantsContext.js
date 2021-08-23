a07f7167edc091bb1a86dbc061ffa6a145c26d74
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-08-23 14:25:10 -0400

    Cleaning up Restaurant context API

--- client/src/context/RetsaurantsContext.js
+++ client/src/context/RetsaurantsContext.js
@@ -1,10 +1,9 @@
 import React, { useState, createContext } from 'react';
-import RestaurantList from '../components/RestaurantList';
 
 export const RestaurantsContext = createContext();
 
 export const RestaurantsContextProvider = props => {
-	const [restaurants, setRestaurants] = useState([]);
+	const [ restaurants, setRestaurants ] = useState([]);
 
 	return (
 		<RestaurantsContext.Provider value={{ restaurants, setRestaurants }}>
