171627fd2105f413c47305f966db51f99c08539c
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-08-24 14:15:39 -0400

    Adding state and handleSubmit function

--- client/src/components/AddRestaurant.jsx
+++ client/src/components/AddRestaurant.jsx
@@ -1,18 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import RestaurantFinder from '../apis/RestaurantFinder';
+import { RestaurantsContext } from '../context/RetsaurantsContext';
 
 const AddRestaurant = () => {
+const [name, setName] = useState('');
+const [location, setLocation] = useState('');
+const [priceRange, setPriceRange] = useState('Price Range');
+
+const handleSubmit = async (e) => {
+	e.preventDefault();
+	const response = await RestaurantFinder.post('/', {
+		name,
+		location,
+		price_range: priceRange
+	})
+	console.log(response);
+};
+
 	return (
 		<div className='my-4'>
 			<form action=''>
 				<div className='row'>
 					<div className='col'>
-						<input type='text' className='form-control' placeholder='name'/>
+						<input
+							type='text'
+							className='form-control'
+							placeholder='Name'
+							value={name}
+							onChange={e => setName(e.target.value)}
+						/>
 					</div>
 					<div className='col'>
-						<input type='text' className='form-control' placeholder='location'/>
+						<input
+							type='text'
+							className='form-control'
+							placeholder='Location'
+							value={location}
+							onChange={e => setLocation(e.target.value)}
+						/>
 					</div>
 					<div className='col'>
-						<select className='form-select mr-sm-2'>
+						<select
+							className='form-select mr-sm-2'
+							value={priceRange}
+							onChange={e => setPriceRange(e.target.value)}
+						>
 							<option disabled>Price Range</option>
 							<option value='1'>$</option>
 							<option value='2'>$$</option>
@@ -21,7 +53,7 @@ const AddRestaurant = () => {
 							<option value='5'>$$$$$</option>
 						</select>
 					</div>
-					<button className='btn btn-primary col-2'>Add</button>
+					<button className='btn btn-primary col-2' onClick={handleSubmit}>Add</button>
 				</div>
 			</form>
 		</div>
