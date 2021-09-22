380b8c10e9606361ed055b29bf8b6a161f858f66
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-09-21 20:30:15 -0400

    Editing review and rating column titles

--- server/db/db.sql
+++ server/db/db.sql
@@ -2,6 +2,6 @@ CREATE TABLE reviews (
 	id BIGSERIAL NOT NULL PRIMARY KEY,
 	restaurant_id BIGINT NOT NULL REFERENCES restaurants(id),
 	name VARCHAR(50) NOT NULL,
-	reviews TEXT NOT NULL,
-	ratings INT NOT NULL check(rating >= 1 and rating <= 5)
-)
+	review TEXT NOT NULL,
+	rating INT NOT NULL check(rating >= 1 and rating <= 5)
+);
