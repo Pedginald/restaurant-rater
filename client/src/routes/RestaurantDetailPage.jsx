e35bed760bc5570fdf95f2dbe46401cecef02493
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-10-12 16:57:14 -0400

    Importing reviews card compnent

--- client/src/routes/RestaurantDetailPage.jsx
+++ client/src/routes/RestaurantDetailPage.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import RestaurantFinder from '../apis/RestaurantFinder';
+import Reviews from '../components/Reviews';
 import StarRating from '../components/StarRating';
 import { RestaurantsContext } from '../context/RestaurantsContext';
 
@@ -23,7 +24,13 @@ const RestaurantDetailPage = () => {
 
 	return (
 		<div>
-			{selectedRestaurant && <StarRating rating={4.5}/>}
+			{selectedRestaurant && (
+				<>
+					<div className="mt-3">
+						<Reviews/>
+					</div>
+				</>
+			)}
 		</div>
 	);
 };
