7cbce2878fd5c295597148d354987cbdc87faa0b
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-10-12 16:35:04 -0400

    Importing the star rating component to detail page

--- client/src/routes/RestaurantDetailPage.jsx
+++ client/src/routes/RestaurantDetailPage.jsx
@@ -1,6 +1,7 @@
 import React, { useContext, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import RestaurantFinder from '../apis/RestaurantFinder';
+import StarRating from '../components/StarRating';
 import { RestaurantsContext } from '../context/RestaurantsContext';
 
 const RestaurantDetailPage = () => {
@@ -22,7 +23,7 @@ const RestaurantDetailPage = () => {
 
 	return (
 		<div>
-			{selectedRestaurant && selectedRestaurant.name}
+			{selectedRestaurant && <StarRating rating={4.5}/>}
 		</div>
 	);
 };
