3fd835a461144c7c21b6006be102c15df6586c43
Peter Rawlings <peterawlings24@hotmail.co.uk>
2021-10-13 14:47:24 -0400

    Layout alteration

--- client/src/components/StarRating.jsx
+++ client/src/components/StarRating.jsx
@@ -6,9 +6,9 @@ const StarRating = ({rating}) => {
 		if (i <= rating) {
 			stars.push(<i className="fas fa-star text-warning"></i>)
 		} else if (i === Math.ceil(rating) && !Number.isInteger(rating)) {
-			stars.push(<i className="fas fa-star-half-alt text-warning"></i>)
+				stars.push(<i className="fas fa-star-half-alt text-warning"></i>)
 		} else {
-			stars.push(<i className="far fa-star text-warning"></i>)
+				stars.push(<i className="far fa-star text-warning"></i>)
 		}
 	}
 
